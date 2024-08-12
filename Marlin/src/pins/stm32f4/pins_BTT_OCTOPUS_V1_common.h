1f776a5d5e352b764aba36fbb5d2079de9849d37
Henrycke Bozza - Parallax <henrycke.bozza@orakolo.com.br>
2024-08-12 15:38:29 -0300

    Update pins_BTT_OCTOPUS_V1_common.h

--- Marlin/src/pins/stm32f4/pins_BTT_OCTOPUS_V1_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_OCTOPUS_V1_common.h
@@ -24,20 +24,16 @@
 #include "env_validate.h"
 
 #define HAS_OTG_USB_HOST_SUPPORT                  // USB Flash Drive support
-#define USES_DIAG_JUMPERS
+// #define USES_DIAG_JUMPERS
 
 // Onboard I2C EEPROM
-#if ANY(NO_EEPROM_SELECTED, I2C_EEPROM)
-  #undef NO_EEPROM_SELECTED
-  #define I2C_EEPROM
-  #define MARLIN_EEPROM_SIZE              0x1000  // 4K (AT24C32)
-  #define SOFT_I2C_EEPROM                         // Force the use of Software I2C
-  #define I2C_SCL_PIN                       PB8
-  #define I2C_SDA_PIN                       PB9
-#endif
+#define I2C_EEPROM
+#define MARLIN_EEPROM_SIZE                0x1000  // 4KB (AT24C32)
+#define I2C_SCL_PIN                         PB8
+#define I2C_SDA_PIN                         PB9
 
 // Avoid conflict with TIMER_TONE
-#define STEP_TIMER 8
+#define STEP_TIMER 10
 
 //
 // Servos
@@ -52,14 +48,21 @@
 //
 // Trinamic Stallguard pins
 //
-#define X_DIAG_PIN                          PG6   // X-STOP
-#define Y_DIAG_PIN                          PG9   // Y-STOP
-#define Z_DIAG_PIN                          PG10  // Z-STOP
+#define X_DIAG_PIN                          PG10 //PG6   // X-STOP
+#define Y_DIAG_PIN                          PG6  //PG9   // Y-STOP
+#define Z_DIAG_PIN                          PG9  //PG10  // Z-STOP
 #define Z2_DIAG_PIN                         PG11  // Z2-STOP
-#define E0_DIAG_PIN                         PG12  // E0DET
+#define E0_DIAG_PIN                         PG11  // E0DET
 #define E1_DIAG_PIN                         PG13  // E1DET
 #define E2_DIAG_PIN                         PG14  // E2DET
 #define E3_DIAG_PIN                         PG15  // E3DET
+
+//
+// Z Probe (when not Z_MIN_PIN)
+//
+#ifndef Z_MIN_PROBE_PIN
+  #define Z_MIN_PROBE_PIN                   PB7
+#endif
 
 //
 // Check for additional used endstop pins
@@ -67,13 +70,13 @@
 #if HAS_EXTRA_ENDSTOPS
   #define _ENDSTOP_IS_ANY(ES) X2_USE_ENDSTOP == ES || Y2_USE_ENDSTOP == ES || Z2_USE_ENDSTOP == ES || Z3_USE_ENDSTOP == ES || Z4_USE_ENDSTOP == ES
   #if _ENDSTOP_IS_ANY(_XMIN_) || _ENDSTOP_IS_ANY(_XMAX_)
-    #define NEEDS_X_MINMAX
+    #define NEEDS_X_MINMAX 1
   #endif
   #if _ENDSTOP_IS_ANY(_YMIN_) || _ENDSTOP_IS_ANY(_YMAX_)
-    #define NEEDS_Y_MINMAX
+    #define NEEDS_Y_MINMAX 1
   #endif
   #if _ENDSTOP_IS_ANY(_ZMIN_) || _ENDSTOP_IS_ANY(_ZMAX_)
-    #define NEEDS_Z_MINMAX
+    #define NEEDS_Z_MINMAX 1
   #endif
   #undef _ENDSTOP_IS_ANY
 #endif
@@ -88,7 +91,7 @@
   #else
     #define X_MIN_PIN                E0_DIAG_PIN  // E0DET
   #endif
-#elif ANY(DUAL_X_CARRIAGE, NEEDS_X_MINMAX)
+#elif EITHER(DUAL_X_CARRIAGE, NEEDS_X_MINMAX)
   #ifndef X_MIN_PIN
     #define X_MIN_PIN                 X_DIAG_PIN  // X-STOP
   #endif
@@ -106,7 +109,7 @@
   #else
     #define Y_MIN_PIN                E1_DIAG_PIN  // E1DET
   #endif
-#elif ENABLED(NEEDS_Y_MINMAX)
+#elif NEEDS_Y_MINMAX
   #ifndef Y_MIN_PIN
     #define Y_MIN_PIN                 Y_DIAG_PIN  // Y-STOP
   #endif
@@ -124,7 +127,7 @@
   #else
     #define Z_MIN_PIN                E2_DIAG_PIN  // PWRDET
   #endif
-#elif ENABLED(NEEDS_Z_MINMAX)
+#elif NEEDS_Z_MINMAX
   #ifndef Z_MIN_PIN
     #define Z_MIN_PIN                 Z_DIAG_PIN  // Z-STOP
   #endif
@@ -135,12 +138,9 @@
   #define Z_STOP_PIN                  Z_DIAG_PIN  // Z-STOP
 #endif
 
-//
-// Z Probe (when not Z_MIN_PIN)
-//
-#ifndef Z_MIN_PROBE_PIN
-  #define Z_MIN_PROBE_PIN                   PB7
-#endif
+#undef NEEDS_X_MINMAX
+#undef NEEDS_Y_MINMAX
+#undef NEEDS_Z_MINMAX
 
 //
 // Filament Runout Sensor
@@ -165,39 +165,34 @@
 #endif
 
 //
-// Misc. Functions
-//
-#define LED_PIN                             PA13
-
-//
 // Steppers
 //
-#define X_STEP_PIN                          PF13  // MOTOR 0
-#define X_DIR_PIN                           PF12
-#define X_ENABLE_PIN                        PF14
-#ifndef X_CS_PIN
-  #define X_CS_PIN                          PC4
-#endif
-
-#define Y_STEP_PIN                          PG0   // MOTOR 1
-#define Y_DIR_PIN                           PG1
-#define Y_ENABLE_PIN                        PF15
+#define Y_STEP_PIN                          PF13  // MOTOR 0
+#define Y_DIR_PIN                           PF12
+#define Y_ENABLE_PIN                        PF14
 #ifndef Y_CS_PIN
-  #define Y_CS_PIN                          PD11
+  #define Y_CS_PIN                          PC4
 #endif
 
-#define Z_STEP_PIN                          PF11  // MOTOR 2
-#define Z_DIR_PIN                           PG3
-#define Z_ENABLE_PIN                        PG5
+#define Z_STEP_PIN                          PG0   // MOTOR 1
+#define Z_DIR_PIN                           PG1
+#define Z_ENABLE_PIN                        PF15
 #ifndef Z_CS_PIN
-  #define Z_CS_PIN                          PC6
+  #define Z_CS_PIN                          PD11
 #endif
 
-#define Z2_STEP_PIN                         PG4   // MOTOR 3
-#define Z2_DIR_PIN                          PC1
-#define Z2_ENABLE_PIN                       PA0
-#ifndef Z2_CS_PIN
-  #define Z2_CS_PIN                         PC7
+#define X_STEP_PIN                          PF11  // MOTOR 2
+#define X_DIR_PIN                           PG3
+#define X_ENABLE_PIN                        PG5
+#ifndef X_CS_PIN
+  #define X_CS_PIN                          PC6
+#endif
+
+#define E1_STEP_PIN                         PG4   // MOTOR 3 //X2
+#define E1_DIR_PIN                          PC1
+#define E1_ENABLE_PIN                       PA0
+#ifndef E1_CS_PIN
+  #define E1_CS_PIN                         PC7
 #endif
 
 #define E0_STEP_PIN                         PF9   // MOTOR 4
@@ -207,11 +202,11 @@
   #define E0_CS_PIN                         PF2
 #endif
 
-#define E1_STEP_PIN                         PC13  // MOTOR 5
-#define E1_DIR_PIN                          PF0
-#define E1_ENABLE_PIN                       PF1
-#ifndef E1_CS_PIN
-  #define E1_CS_PIN                         PE4
+#define Z2_STEP_PIN                         PC13  // MOTOR 5
+#define Z2_DIR_PIN                          PF0
+#define Z2_ENABLE_PIN                       PF1
+#ifndef Z2_CS_PIN
+  #define Z2_CS_PIN                         PE4
 #endif
 
 #define E2_STEP_PIN                         PE2   // MOTOR 6
@@ -246,7 +241,7 @@
 #define HEATER_2_PIN                        PB10  // Heater2
 #define HEATER_3_PIN                        PB11  // Heater3
 
-#define FAN0_PIN                            PA8   // Fan0
+#define FAN_PIN                             PA8   // Fan0
 #define FAN1_PIN                            PE5   // Fan1
 #define FAN2_PIN                            PD12  // Fan2
 #define FAN3_PIN                            PD13  // Fan3
@@ -265,16 +260,18 @@
 #endif
 
 //
-// SPI pins for TMC2130 stepper drivers
+// Software SPI pins for TMC2130 stepper drivers
 //
-#ifndef TMC_SPI_MOSI
-  #define TMC_SPI_MOSI                      PA7
-#endif
-#ifndef TMC_SPI_MISO
-  #define TMC_SPI_MISO                      PA6
-#endif
-#ifndef TMC_SPI_SCK
-  #define TMC_SPI_SCK                       PA5
+#if ENABLED(TMC_USE_SW_SPI)
+  #ifndef TMC_SW_MOSI
+    #define TMC_SW_MOSI                     PA7
+  #endif
+  #ifndef TMC_SW_MISO
+    #define TMC_SW_MISO                     PA6
+  #endif
+  #ifndef TMC_SW_SCK
+    #define TMC_SW_SCK                      PA5
+  #endif
 #endif
 
 #if HAS_TMC_UART
@@ -321,11 +318,8 @@
   #define E3_SERIAL_RX_PIN      E3_SERIAL_TX_PIN
 
   // Reduce baud rate to improve software serial reliability
-  #ifndef TMC_BAUD_RATE
-    #define TMC_BAUD_RATE                  19200
-  #endif
-
-#endif // HAS_TMC_UART
+  #define TMC_BAUD_RATE                    19200
+#endif
 
 /**               ------                                      ------
  * (BEEPER) PE8  | 1  2 | PE7  (BTN_ENC)         (MISO) PA6  | 1  2 | PA5  (SCK)
@@ -359,7 +353,7 @@
 // Must use soft SPI because Marlin's default hardware SPI is tied to LCD's EXP2
 //
 #if SD_CONNECTION_IS(ONBOARD)
-  #define ONBOARD_SDIO                            // Use SDIO for onboard SD
+  #define SDIO_SUPPORT                            // Use SDIO for onboard SD
   #ifndef SD_DETECT_STATE
     #define SD_DETECT_STATE HIGH
   #elif SD_DETECT_STATE == LOW
@@ -427,38 +421,13 @@
 #endif // BTT_MOTOR_EXPANSION
 
 //
-// LCD / Controller
+// LCDs and Controllers
 //
-
 #if IS_TFTGLCD_PANEL
 
   #if ENABLED(TFTGLCD_PANEL_SPI)
     #define TFTGLCD_CS               EXP2_03_PIN
   #endif
-
-#elif HAS_DWIN_E3V2 || IS_DWIN_MARLINUI
-  /**
-   *        ------                 ------            ---
-   *       | 1  2 |               | 1  2 |            1 |
-   *       | 3  4 |            RX | 3  4 | TX       | 2 | RX
-   *   ENT   5  6 | BEEP      ENT   5  6 | BEEP     | 3 | TX
-   *     B | 7  8 | A           B | 7  8 | A        | 4 |
-   *   GND | 9 10 | VCC       GND | 9 10 | VCC        5 |
-   *        ------                 ------            ---
-   *         EXP1                   DWIN             TFT
-   *
-   * DWIN pins are labeled as printed on DWIN PCB. GND, VCC, A, B, ENT & BEEP can be connected in the same
-   * orientation as the existing plug/DWIN to EXP1. TX/RX need to be connected to the TFT port, with TX->RX, RX->TX.
-   */
-
-  #ifndef NO_CONTROLLER_CUSTOM_WIRING_WARNING
-    #error "CAUTION! Ender-3 V2 display requires a custom cable. See 'pins_BTT_OCTOPUS_V1_common.h' for details. (Define NO_CONTROLLER_CUSTOM_WIRING_WARNING to suppress this warning.)"
-  #endif
-
-  #define BEEPER_PIN                 EXP1_06_PIN
-  #define BTN_EN1                    EXP1_08_PIN
-  #define BTN_EN2                    EXP1_07_PIN
-  #define BTN_ENC                    EXP1_05_PIN
 
 #elif HAS_WIRED_LCD
 
@@ -472,7 +441,7 @@
     #define BTN_EN1                  EXP1_03_PIN
     #define BTN_EN2                  EXP1_05_PIN
 
-    #define LCD_PINS_EN              EXP1_08_PIN
+    #define LCD_PINS_ENABLE          EXP1_08_PIN
     #define LCD_PINS_D4              EXP1_06_PIN
 
   #else
@@ -482,7 +451,7 @@
     #define BTN_EN1                  EXP2_03_PIN
     #define BTN_EN2                  EXP2_05_PIN
 
-    #define LCD_PINS_EN              EXP1_03_PIN
+    #define LCD_PINS_ENABLE          EXP1_03_PIN
     #define LCD_PINS_D4              EXP1_05_PIN
 
     #if ENABLED(FYSETC_MINI_12864)
@@ -490,7 +459,7 @@
       #define DOGLCD_A0              EXP1_04_PIN
       //#define LCD_BACKLIGHT_PIN           -1
       #define LCD_RESET_PIN          EXP1_05_PIN  // Must be high or open for LCD to operate normally.
-      #if ANY(FYSETC_MINI_12864_1_2, FYSETC_MINI_12864_2_0)
+      #if EITHER(FYSETC_MINI_12864_1_2, FYSETC_MINI_12864_2_0)
         #ifndef RGB_LED_R_PIN
           #define RGB_LED_R_PIN      EXP1_06_PIN
         #endif
@@ -517,8 +486,7 @@
     #endif
 
   #endif
-
-#endif // HAS_WIRED_LCD
+#endif  // HAS_WIRED_LCD
 
 // Alter timing for graphical display
 #if IS_U8GLIB_ST7920
@@ -552,28 +520,26 @@
   #define NEOPIXEL_PIN                      PB0
 #endif
 
-#if ENABLED(WIFISUPPORT)
-  //
-  // WIFI
-  //
+//
+// WIFI
+//
 
-  /**
-   *                      -------
-   *            GND | 9  |       | 8 | 3.3V
-   *  (ESP-CS) PB12 | 10 |       | 7 | PB15 (ESP-MOSI)
-   *           3.3V | 11 |       | 6 | PB14 (ESP-MISO)
-   * (ESP-IO0)  PD7 | 12 |       | 5 | PB13 (ESP-CLK)
-   * (ESP-IO4) PD10 | 13 |       | 4 | --
-   *             -- | 14 |       | 3 | PE15 (ESP-EN)
-   *  (ESP-RX)  PD8 | 15 |       | 2 | --
-   *  (ESP-TX)  PD9 | 16 |       | 1 | PE14 (ESP-RST)
-   *                      -------
-   *                       WIFI
-   */
-  #define ESP_WIFI_MODULE_COM                  3  // Must also set either SERIAL_PORT or SERIAL_PORT_2 to this
-  #define ESP_WIFI_MODULE_BAUDRATE      BAUDRATE  // Must use same BAUDRATE as SERIAL_PORT & SERIAL_PORT_2
-  #define ESP_WIFI_MODULE_RESET_PIN         PG7
-  #define ESP_WIFI_MODULE_ENABLE_PIN        PG8
-  #define ESP_WIFI_MODULE_GPIO0_PIN         PD7
-  #define ESP_WIFI_MODULE_GPIO4_PIN         PD10
-#endif
+/**
+ *                      -------
+ *            GND | 9  |       | 8 | 3.3V
+ *  (ESP-CS) PB12 | 10 |       | 7 | PB15 (ESP-MOSI)
+ *           3.3V | 11 |       | 6 | PB14 (ESP-MISO)
+ * (ESP-IO0)  PD7 | 12 |       | 5 | PB13 (ESP-CLK)
+ * (ESP-IO4) PD10 | 13 |       | 4 | --
+ *             -- | 14 |       | 3 | PE15 (ESP-EN)
+ *  (ESP-RX)  PD8 | 15 |       | 2 | --
+ *  (ESP-TX)  PD9 | 16 |       | 1 | PE14 (ESP-RST)
+ *                      -------
+ *                       WIFI
+ */
+#define ESP_WIFI_MODULE_COM                    3  // Must also set either SERIAL_PORT or SERIAL_PORT_2 to this
+#define ESP_WIFI_MODULE_BAUDRATE        BAUDRATE  // Must use same BAUDRATE as SERIAL_PORT & SERIAL_PORT_2
+#define ESP_WIFI_MODULE_RESET_PIN           PG7
+#define ESP_WIFI_MODULE_ENABLE_PIN          PG8
+#define ESP_WIFI_MODULE_GPIO0_PIN           PD7
+#define ESP_WIFI_MODULE_GPIO4_PIN           PD10
